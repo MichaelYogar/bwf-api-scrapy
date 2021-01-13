ada98b74dce27999a3447a396013c570212fd079
michaelyogar <myogar@uoguelph.ca>
2021-01-13 17:53:48 -0500

    query file

---
+++ src/db/queries.ts
@@ -0,0 +1,8 @@
+/** GET ALL DATA  */
+let queryMS = "SELECT * FROM `MEN'S SINGLES`";
+let queryMD = "SELECT * FROM `MEN'S DOUBLES`";
+let queryWS = "SELECT * FROM `WOMEN'S SINGLES`";
+let queryWD = "SELECT * FROM `WOMEN'S DOUBLES`";
+let queryMX = 'SELECT * FROM `MIXED DOUBLES`';
+
+export { queryMS, queryMD, queryWS, queryWD, queryMX };
